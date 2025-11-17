4770ba55612c8fbc5d2051415eaf4d6b288a1fb9
Marin-B-git <mrnbilac@gmail.com>
2025-11-17 09:28:11 -0800

    Create SentinelSum.java

---
+++ SentinelSum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SentinelSum {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.print("Say Number");
+      int number1 = scan.nextInt();
+      System.out.print("Say Number");
+      int number2 = scan.nextInt();
+      System.out.print("Say Number");
+      int number3 = scan.nextInt();
+      System.out.print("Say Number");
+      int number4 = scan.nextInt();
+      System.out.print("Say Number");
+      int number5 = scan.nextInt();
+
+      
+      int sum = number1 + number2 + number3 + number4 + number5;
+      
+
+      System.out.println("The sum is " + sum);
+         
+         
+       
+   }
+}
+   
